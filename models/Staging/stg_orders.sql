39e09bf0a6b7cf1030b8eb53bd228a47c458f31c
prateek.agarwal@tredence.com <prateek.agarwal@tredence.com>
2024-10-23 07:44:42 +0000

    Dev staging

---
+++ models/Staging/stg_orders.sql
@@ -0,0 +1,24 @@
+ select
+--from raw_orders
+orderid,
+orderdate,
+shipdate,
+shipmode,
+o.customerid,
+o.productid,
+ordersellingprice,
+ordercostprice,
+--from raw_customer
+customername,
+segment,
+country,
+--from raw_product
+category,
+productname,
+subcategory,
+ordersellingprice - ordercostprice as orderprofit
+from {{ ref('Orders') }} as o
+left join {{ ref('customers') }} as c
+on o.customerid = c.customerid
+left join {{ ref('product') }} as p
+on o.productid = p.productid
